7617f3b0be2033c4c1a6a54a45506fc7e4b77144
yarik <super.racing@ukr.net>
2025-09-22 15:07:57 +0300

    removed ReadOnlyModelViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from drf_spectacular.utils import (
     extend_schema, OpenApiParameter, OpenApiExample
 )
